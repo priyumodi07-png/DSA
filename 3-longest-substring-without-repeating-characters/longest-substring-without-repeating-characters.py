4b1fb24369b42404076fec2dfb81f5b3d46eec43
priyumodi07 <priyumodi07@gmail.com>
2026-07-16 22:23:04 +0530

    Time: 19 ms (78.49%) | Memory: 13.2 MB (23.06%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        seen = {}
+        left = 0
+        max_len = 0
+
+        for right in range(len(s)):
+            if s[right] in seen and seen[s[right]] >= left:
+                left = seen[s[right]] + 1
+            seen[s[right]] = right
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
