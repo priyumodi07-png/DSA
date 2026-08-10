c67ff1709f8e49bf977ffc403aab0d6d196464ca
priyumodi07 <priyumodi07@gmail.com>
2026-08-10 12:25:59 +0530

    Time: 7 ms (20.26%) | Memory: 47.1 MB (36.47%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -1,20 +1,14 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-       Arrays.sort(nums);
-        List<Integer>ans=new ArrayList<>();
-        int end=nums[nums.length-1],index=0,cur=nums[0];
-        while(cur<end){
-            if(nums[index]==cur){
-                cur++;
-                index++;
-                continue;
-            }
-            else{
-                while(cur<nums[index]){
-                    ans.add(cur++);
-                }
-                index++;
-                cur++;
+        Arrays.sort(nums);
+        List<Integer> ans = new ArrayList<>();
+        
+        for (int i = 1; i < nums.length; i++) {
+            // If there is a gap between nums[i-1] and nums[i]
+            for (int val = nums[i-1] + 1; val < nums[i]; val++) {
+                ans.add(val);
             }
         }
         return ans;
