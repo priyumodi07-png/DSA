0aa534e30866075109f0d372a856119394977d92
priyumodi07 <priyumodi07@gmail.com>
2026-08-26 12:51:21 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (87.25%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,30 +1,53 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
+        if(s.length()<k) return "";
+
         int n = s.length();
-        int val = Integer.MAX_VALUE;
-        String ans = "";
-        for(int i=0;i<n;i++){
-            int ones = 0;
-            String check = "";
-            int count1 = 0;
-            for(int j=i;j<n;j++){
-                char ch = s.charAt(j);
-                check+=ch;
-                
-                if(ch=='1'){
-                    ones++;
-                }
-                if(ones>k){
-                    break;
-                }
-                if(ones==k){
-                    if(check.length()<val || check.length()==val && check.compareTo(ans)<0){
-                        val = check.length();
-                        ans = check;
+
+        int l = 0;
+        int cnt = 0;
+        int mini = Integer.MAX_VALUE;
+        int start = -1;
+
+        for(int r=0;r<n;r++){
+            char c = s.charAt(r);
+
+            if(c=='1') cnt++;
+
+            while(cnt==k){
+
+                if((r-l+1)==mini){
+                    // same length, keep the smaller one
+                    if(check(s, start, l, mini)){
+                        start = l;
                     }
                 }
+                else if((r-l+1)<mini){
+                    mini = r-l+1;
+                    start = l;
+                }
+
+                if(s.charAt(l)=='1') cnt--;
+                l++;
             }
         }
-        return ans;
+
+        return start==-1 ? "" : s.substring(start, start+mini);
+    }
+
+    // true if window at 'start' is bigger than window at 'l'
+    private boolean check(String s, int start, int l, int mini){
+        int n = start+mini;
+        int m = l+mini;
+
+        while(start<n && l<m){
+            if(s.charAt(start) > s.charAt(l)) return true;
+            if(s.charAt(start) < s.charAt(l)) return false;
+
+            start++;
+            l++;
+        }
+
+        return false;
     }
 }
